d892f437d3fdcf1dd07f20ae3a360b75941abb6b
sjoulbak <remco_meinen@hotmail.com>
2021-05-25 13:55:15 +0200

    Added support for zlib and png

---
+++ lib/png_functions.sh
@@ -0,0 +1,52 @@
+function install_png() {
+  PNG_CACHE_FILE="$CACHE_DIR/png.tar.gz"
+  PNG_INSTALL_DIR="$VENDOR_DIR/png"
+  PNG_PROFILE_PATH="$BUILD_DIR/.profile.d/png.sh"
+  PNG_RUNTIME_INSTALL_PATH="$HOME/vendor/png"
+
+  mkdir -p $PNG_INSTALL_DIR
+
+  if [ ! -f $PNG_CACHE_FILE ]; then
+    echo "-----> Downloading zlib version 1.2.11"
+    wget "http://www.zlib.net/zlib-1.2.11.tar.gz"
+
+    echo "-----> Extracting zlib"
+    tar xvzf "zlib-1.2.11.tar.gz"
+
+    echo "-----> Building zlib"
+    cd "zlib-1.2.11"
+    ./configure --prefix=$PNG_INSTALL_DIR
+    make
+    make install
+    cd ..
+    rm -rf "zlib-1.2.11"
+
+    echo "-----> Downloading png version 1.6.37"
+    wget "https://download.sourceforge.net/libpng/libpng-1.6.37.tar.gz"
+
+    echo "-----> Extracting png"
+    tar xvzf "libpng-1.6.37.tar.gz"
+
+    echo "-----> Building png"
+    cd "libpng-1.6.37"
+    ./configure --prefix=$PNG_INSTALL_DIR
+    make
+    make install
+    cd ..
+    rm -rf "libpng-1.6.37"
+
+    echo "-----> Caching png"
+    cd $VENDOR_DIR
+    tar czf png.tar.gz png
+
+    mv png.tar.gz $PNG_CACHE_FILE
+  else
+    echo "-----> Extracting png from cache"
+    tar xzf $PNG_CACHE_FILE -C $PNG_INSTALL_DIR
+  fi
+
+  echo "-----> Building runtime environment"
+  mkdir -p $(dirname $PNG_PROFILE_PATH)
+  echo "export PATH=$PNG_RUNTIME_INSTALL_PATH/bin:\$PATH" >> $PNG_PROFILE_PATH
+  echo "export LD_LIBRARY_PATH=$PNG_RUNTIME_INSTALL_PATH/lib:\$LD_LIBRARY_PATH" >> $PNG_PROFILE_PATH
+}
