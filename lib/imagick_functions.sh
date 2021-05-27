e51a8836439dc130b8d97a27d671c8827bd7b589
sjoulbak <remco_meinen@hotmail.com>
2021-05-27 08:03:08 +0200

    Imagick should be downloaded from github, due to PHP 8 issues

--- lib/imagick_functions.sh
+++ lib/imagick_functions.sh
@@ -3,27 +3,23 @@ function install_imagick() {
   IMAGICK_INSTALL_DIR="$VENDOR_DIR/imagick"
   IMAGICK_PROFILE_PATH="$BUILD_DIR/.profile.d/imagick.sh"
   IMAGICK_RUNTIME_INSTALL_PATH="$HOME/vendor/imagick"
-  IMAGICK_VERSION=3.4.4
   PHP_EXT_DIR=$(php-config --extension-dir)
   IMAGICK_INSTALL_FILE="$PHP_EXT_DIR/imagick.so"
 
   mkdir -p $IMAGICK_INSTALL_DIR
 
   if [ ! -f $IMAGICK_CACHE_FILE ]; then
-    echo "-----> Downloading imagick version $IMAGICK_VERSION"
-    wget "https://pecl.php.net/get/imagick-$IMAGICK_VERSION.tgz"
-
-    echo "-----> Extracting imagick"
-    tar xvzf "imagick-$IMAGICK_VERSION.tgz"
+    echo "-----> Downloading imagick version https://github.com/Imagick/imagick"
+    git clone https://github.com/Imagick/imagick
 
     echo "-----> Building imagick"
-    cd "imagick-$IMAGICK_VERSION"
+    cd "imagick"
     phpize
     ./configure --prefix=$IMAGICK_INSTALL_DIR --with-imagick=$VENDOR_DIR/image_magick
     make
     mv modules/imagick.so $IMAGICK_INSTALL_FILE
     cd ..
-    rm -rf "imagick-$IMAGICK_VERSION"
+    rm -rf "imagick"
 
     echo "-----> Caching imagick"
     cp $IMAGICK_INSTALL_FILE $IMAGICK_CACHE_FILE
